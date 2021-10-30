a5be4df8098de40ad72f11866d70b72ac16ecf73
Brian Siklinski <bszyk.codes@gmail.com>
2021-10-30 18:29:54 -0400

    deleted redundant components

--- src/register/registerPlugin.jsx
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@wordpress/plugins';
-import DynamicContentPlugin from '../components/index.jsx'
-
-// registerPlugin('bszyk-dynamic-content', {
-//   icon: 'database',
-//   render: () => <DynamicContentPlugin />
-// });
