7361aa0fca3d06f8c4db762fcfc6dc4fa8d1d0a4
Brian Siklinski <bszyk.codes@gmail.com>
2021-11-07 15:54:41 -0500

    mostly formatting

--- zip.sh
+++ zip.sh
@@ -1,23 +1,19 @@
-
-#! /bin/bash
 set -e
+#! /bin/bash
 
 echo "" &&
-echo "=======================================================" &&
-echo "*        Build a ZIP for Dynamic Content              *" &&
-echo "*               pls use Bash ^4                       *" &&
-echo "=======================================================" &&
-echo "" &&
+    echo "=======================================================" &&
+    echo "*        Build a ZIP for Dynamic Content              *" &&
+    echo "*               pls use Bash ^4                       *" &&
+    echo "=======================================================" &&
+    echo ""
 
 # chmod 775
 
-#Ask & Store Version
-read -r -p "Version Number: " VERSION
-
-# VARIABLES
+# Variables
 PLUGIN="dynamic-content"
 ZIP_FOLDER="_zip"
-DONE="🎉 done!\n"
+DONE="\n🎉 done!\n"
 
 #############################################################
 # Grab the script from an existing file -or- user input...  #
@@ -27,9 +23,8 @@ DONE="🎉 done!\n"
 # From: https://github.com/hopeseekr/BashScripts/           #
 # @see https://stackoverflow.com/a/64486155/430062          #
 #############################################################
-function getChangelog()
-{
-    if [ ! -z "$1" ] &&  [ -f "$1" ]; then
+function getChangelog() {
+    if [ ! -z "$1" ] && [ -f "$1" ]; then
         echo $(<"$1")
     else
         echo "" >&2
@@ -46,26 +41,48 @@ function getChangelog()
     fi
 }
 
+# Ask and store version
+read -r -p "Version: " VERSION
+
+# Make _zip folder
+mkdir $ZIP_FOLDER || true
+
+# Define zip path
+ZIP_FILE="$PLUGIN-v$VERSION.zip"
+
+# Offer overwrite option if zip version already exists
+if [ -f "$ZIP_FOLDER/$ZIP_FILE" ]; then
+    while true; do
+        echo "$ZIP_FOLDER/$ZIP_FILE already exists. Overwrite? (y/n) "
+        read -p "(Note: You will have to manually fixup the changelog.) " yn
+        case $yn in
+        [Yy]*) break ;;
+        [Nn]*) exit ;;
+        *) echo "Please answer yes or no." ;;
+        esac
+    done
+fi
+
+# Run the function above to make a changelog list with formatted newlines
 CHANGELOG=$(getChangelog "$1")
 
-echo "\n📝 updating changelog..."
-echo -e "\n= $VERSION =\n$CHANGELOG\n$(cat changelog.txt)" > changelog.txt
+# Append to top of changelog.txt
+echo -e "\n📝 updating changelog..."
+echo -e "\n= $VERSION =\n$CHANGELOG\n$(cat changelog.txt)" >changelog.txt
 cat changelog.txt
 echo -e $DONE
 
-# Run npm build to create new dist files 
-echo "⛏  building dist files..."
+# Run npm build to create new dist files
+echo -e "\n⛏  building dist files..."
 npm run build
 echo -e $DONE
 
-# make new zip folder and zip file with build dist files
-echo "🧪 creating zip file..."
-mkdir $ZIP_FOLDER
-ZIP_FILE="$PLUGIN-v$VERSION.zip"
+# Make new zip folder and zip file with build dist files
+echo -e "\n🧪 creating zip file...\n"
 find . -path ./node_modules -prune -o -name "*.php" -print | zip $ZIP_FOLDER/$ZIP_FILE -@
 zip -ur $ZIP_FOLDER/$ZIP_FILE assets build changelog.txt constants.json readme.txt readme.md LICENSE
 echo -e $DONE
 
-echo "🤘 $ZIP_FOLDER/$ZIP_FILE created!"
+echo -e "\n🤘 $ZIP_FOLDER/$ZIP_FILE created!\n"
 
-# that's all!
+# That's all!
