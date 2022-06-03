357aac6416c764de0e5cd9179cb9fddf1a129ba7
Brian Siklinski <bszyk.codes@gmail.com>
2022-06-03 18:29:17 -0400

    get content from post global

--- inc/ProcessBlocks.php
+++ inc/ProcessBlocks.php
@@ -7,25 +7,28 @@
 
 namespace DYNAMIC_BLOCK_CONTENT;
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
 /**
  * Process blocks.
  */
-class ProcessBlocks {
+class ProcessBlocks
+{
 	/**
 	 * Construct.
 	 */
-	public function __construct() {
+	public function __construct()
+	{
 	}
 
 	/**
 	 * Init.
 	 */
-	public function init() {
-		add_action( 'the_content', array( $this, 'process_blocks' ), 0, 1 );
+	public function init()
+	{
+		add_action('the_content', array($this, 'process_blocks'), 0, 1);
 	}
 
 	/**
@@ -34,48 +37,51 @@ class ProcessBlocks {
 	 * @param string $content Serialized post content.
 	 * @return string $content Altered and serialized post content.
 	 */
-	public function process_blocks( $content ) {
+	public function process_blocks()
+	{
 		global $post;
 
-		$blocks = parse_blocks( $content );
+		$blocks = parse_blocks($post->post_content);
 
-		foreach ( $blocks as &$block ) {
+		foreach ($blocks as &$block) {
 			// check if it's in the allow list.
-			if ( isset( $block['blockName'] ) && in_array( $block['blockName'], ALLOWED_BLOCKS, true ) ) {
+			if (isset($block['blockName']) && in_array($block['blockName'], ALLOWED_BLOCKS, true)) {
+
 				// check if it's a dynamic content block.
-				if ( isset( $block['attrs']['dbc_metakey'] ) && isset( $block['attrs']['dbc_enabled'] ) && true === $block['attrs']['dbc_enabled'] ) {
+				if (isset($block['attrs']['dbc_metakey']) && isset($block['attrs']['dbc_enabled']) && true === $block['attrs']['dbc_enabled']) {
+
 					// define the meta key.
 					$meta_key = $block['attrs']['dbc_metakey'];
 
 					// grab the inner content.
 					$inner_content = $block['innerContent'][0];
 
-					if ( ! empty( $meta_key ) && metadata_exists( 'post', $post->ID, $meta_key ) ) {
+					if (!empty($meta_key) && metadata_exists('post', $post->ID, $meta_key)) {
 						// get the current meta value.
-						$meta_value = get_post_meta( $post->ID, $meta_key, true );
+						$meta_value = get_post_meta($post->ID, $meta_key, true);
 
-						if ( empty( $meta_value ) ) {
+						if (empty($meta_value)) {
 							$meta_value = '[[Dynamic Content Warning: Value of `' . $meta_key . '` is empty.]]';
 						}
 
 						// replace generic string with current meta.
-						$inner_content = str_replace( THE_DYNAMIC_CONTENT_STRING, $meta_value, $inner_content );
-					} elseif ( empty( $meta_key ) ) {
+						$inner_content = str_replace(THE_DYNAMIC_CONTENT_STRING, $meta_value, $inner_content);
+					} elseif (empty($meta_key)) {
 						// if empty, replace with error.
 						// @TODO settings option to turn debugging messages on/off
-						$inner_content = str_replace( THE_DYNAMIC_CONTENT_STRING, '[[Dynamic Content Warning: Metadata key is empty.]]', $inner_content );
-					} elseif ( ! metadata_exists( 'post', $post->ID, $meta_key ) ) {
-						$inner_content = str_replace( THE_DYNAMIC_CONTENT_STRING, '[[Dynamic Content Error: Metadata key `' . $meta_key . '` no longer exists.]]', $inner_content );
+						$inner_content = str_replace(THE_DYNAMIC_CONTENT_STRING, '[[Dynamic Content Warning: Metadata key is empty.]]', $inner_content);
+					} elseif (!metadata_exists('post', $post->ID, $meta_key)) {
+						$inner_content = str_replace(THE_DYNAMIC_CONTENT_STRING, '[[Dynamic Content Error: Metadata key `' . $meta_key . '` no longer exists.]]', $inner_content);
 					} else {
-						$inner_content = str_replace( THE_DYNAMIC_CONTENT_STRING, '[[Dynamic Content Error: Congrats! 🎉 You found a new error!]]', $inner_content );
+						$inner_content = str_replace(THE_DYNAMIC_CONTENT_STRING, '[[Dynamic Content Error: Congrats! 🎉 You found a new error!]]', $inner_content);
 					}
-						// update the block.
-						$block['innerContent'][0] = $inner_content;
+					// update the block.
+					$block['innerContent'][0] = $inner_content;
 				}
 			}
 		}
 
-		$content = serialize_blocks( $blocks );
+		$content = serialize_blocks($blocks);
 
 		return $content;
 	}
