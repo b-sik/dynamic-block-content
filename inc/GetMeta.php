05801b3d75e536cb759b49688d18c215f5f7c812
Brian Siklinski <bszyk.codes@gmail.com>
2022-06-11 11:07:12 -0400

    filter out serialized values

--- inc/GetMeta.php
+++ inc/GetMeta.php
@@ -23,21 +23,23 @@ class GetMeta {
 
 	/**
 	 * Localize meta.
+	 * @return void
 	 */
 	public function localize() {
 		wp_localize_script(
 			'bszyk-dynamic-content-scripts',
 			'dynamicContent',
 			array(
-				'postMeta' => $this->post(),
+				'postMeta' => $this->get_filtered_post_meta(),
 			)
 		);
 	}
 
 	/**
-	 * Get post meta.
+	 * Get post meta after running through custom filters.
+	 * @return string $post_meta
 	 */
-	public function post() {
+	public function get_filtered_post_meta() {
 		global $post;
 
 		if ( null === $post ) {
@@ -45,16 +47,49 @@ class GetMeta {
 		}
 
 		$post_meta = get_post_meta( $post->ID );
+		$post_meta = $this->apply_post_meta_filters($post_meta);
 
-		// filter out private keys.
-		$post_meta = array_filter(
+		return $post_meta;
+	}
+
+	/**
+	 * Apply custom filters.
+	 * @param array $post_meta
+	 * @return array
+	 */
+	public function apply_post_meta_filters( $post_meta ) {
+		$post_meta = $this->filter_private_keys( $post_meta );
+		$post_meta = $this->filter_serialized_values( $post_meta );
+
+		return $post_meta;
+	}
+
+	/**
+	 * Filter out keys that being with underscore.
+	 * @param array $post_meta
+	 * @return array $post_meta
+	 */
+	public function filter_private_keys( $post_meta ) {
+		return array_filter(
 			$post_meta,
 			function( $key ) {
 				return substr_compare( $key, '_', 0, 1 ) !== 0;
 			},
 			ARRAY_FILTER_USE_KEY
 		);
+	}
 
-		return $post_meta;
+	/**
+	 * Filter out serialized values.
+	 * @param array $post_meta
+	 * @return array $post_meta
+	 */
+	public function filter_serialized_values( $post_meta ) {
+		return array_filter(
+			$post_meta,
+			function( $value ) {
+				return ! is_serialized( $value[0] );
+			},
+		);
 	}
 }
